a6f2109bad5666a9d22a85f27903b879c03bd5e5
Andrew Myhre <andrew.myhre@gmail.com>
2016-08-22 10:13:08 -0400

    trying to fix owain startup error

--- src/asp.net-mvc/massive-moose.api/Properties/AssemblyInfo.cs
+++ src/asp.net-mvc/massive-moose.api/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("massive_moose.server.mvc")]
+[assembly: AssemblyTitle("massive_moose.api")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("massive_moose.server.mvc")]
+[assembly: AssemblyProduct("massive_moose.api")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
