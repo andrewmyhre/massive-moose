75d24f1f5fce522d7da5e70cfee9be63079c3038
Andrew Myhre <andrew.myhre@gmail.com>
2016-08-22 10:06:01 -0400

    adds unhandled exception logging

--- src/asp.net-mvc/massive-moose.api/Global.asax.cs
+++ src/asp.net-mvc/massive-moose.api/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Http;
+﻿using log4net;
+using System;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -7,6 +10,7 @@ namespace massive_moose.server.api
 {
     public class WebApiApplication : System.Web.HttpApplication
     {
+        private static ILog Log = LogManager.GetLogger(typeof(WebApiApplication));
         protected void Application_Start()
         {
             log4net.Config.XmlConfigurator.Configure();
@@ -15,6 +19,42 @@ namespace massive_moose.server.api
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            if (exception != null)
+            {
+                Log.Error("Unhandled exception", exception);
+            }
+
+            HttpException httpException = exception as HttpException;
+            if (httpException != null)
+            {
+                RouteData routeData = new RouteData();
+                routeData.Values.Add("controller", "Error");
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        // page not found
+                        //routeData.Values.Add("action", "HttpError404");
+                        break;
+                    case 500:
+                        // server error
+                        //routeData.Values.Add("action", "HttpError500");
+                        break;
+                    default:
+                        routeData.Values.Add("action", "Index");
+                        break;
+                }
+                routeData.Values.Add("error", exception.Message);
+                // clear error on server
+                Server.ClearError();
+
+                //Response.RedirectToRoute(routeData.Values);
+            }
+            Server.ClearError();
         }
     }
 }
