cf17579a31d37f4fb041102c09f0112bb083ad1a
Andrew Myhre <andrew.myhre@gmail.com>
2016-09-06 15:15:35 -0400

    adds terms of use and privacy policy pages

---
+++ src/asp.net-mvc/massive-moose.web.owin/Controllers/LegalController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace massive_moose.web.owin.Controllers
+{
+    public class LegalController : Controller
+    {
+        // GET: Legal
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        public ActionResult TermsOfUse()
+        {
+            return View();
+        }
+    }
+}
