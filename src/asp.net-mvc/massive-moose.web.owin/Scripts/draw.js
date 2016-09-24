41f42afce8fbdb6a76c38e9f956a5238fafc270e
Andrew Myhre <andrew.myhre@gmail.com>
2016-09-24 17:52:53 -0400

    force re deploy

--- src/asp.net-mvc/massive-moose.web.owin/Scripts/draw.js
+++ src/asp.net-mvc/massive-moose.web.owin/Scripts/draw.js
@@ -1,4 +1,4 @@
-﻿var utils = {};
+﻿var utils = {}; 
 
 utils.distanceBetween = function (point1, point2) {
     return Math.sqrt(Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2));
